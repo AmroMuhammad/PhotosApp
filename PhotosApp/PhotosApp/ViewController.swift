a0023475f9e2b177e896ef73523f1588c3287ece
AmrMuhammad <amrmu94@gmail.com>
2021-12-10 11:44:23 -0800

    initial commit

---
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotosApp
+//
+//  Created by Amr Muhammad on 12/10/21.
+//  Copyright © 2021 Amr Muhammad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
